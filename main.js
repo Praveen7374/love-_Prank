208d694542e3a8d259a15d14e761a2321066d0f3
Praveen <149293178+Praveen7374@users.noreply.github.com>
2023-12-22 12:53:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ $btnNo.addEventListener('mouseover', (e) =>{
     $target.style.right = `${Math.random() * 200}px`
 
     if(counter >= 5) {
-        $target.innerHTML = 'HAHAHAAHA'
+        $target.innerHTML = 'मना नहीं करने दूंगा'
     }
 
     counter += 1
